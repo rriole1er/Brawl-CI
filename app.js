d7c402f2695fca1dfbdd97a43636636b56890394
Remyto667 <remyriole@hotmail.fr>
2023-10-01 12:27:07 +0200

    feat : fetch tag

--- app.js
+++ app.js
@@ -6,26 +6,30 @@ app.set('view engine', 'ejs');
 
 
 
-data = {} 
+stats = {} 
 
 app.get('/', async (req, res) => {
-
-  // try {
-  //     const response = await fetch("https://api.brawlstars.com/v1/players/%23VUGVJYUY", {
-  //         method: 'GET',
-  //         headers: {
-  //             Authorization: 'Bearer REDACTED_API_KEY',
-  //             Accept: 'application/json'
-  //         }
-  //     });
-  //     data = await response.json();
-
-  // } catch (error) {
-  //     console.error(error);
-  //     res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
-  // }
-
   res.render('vue');
+});
+
+app.get('/api', async (req, res) => {
+
+   try {
+       const response = await fetch("https://api.brawlstars.com/v1/players/%23VUGVJYUY", {
+           method: 'GET',
+           headers: {
+               Authorization: 'Bearer REDACTED_API_KEY',
+               Accept: 'application/json'
+           }
+       });
+       stats = await response.json();
+
+   } catch (error) {
+       console.error(error);
+      res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
+   }
+
+  res.render('vue',{data:stats});
 });
 
 app.listen(port, () => {
