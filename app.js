cb2b688fbb550cc7c0f87cd081f391e83a1d42f2
Remyto667 <remyriole@hotmail.fr>
2023-10-01 12:27:07 +0200

    feat : fetch tag

--- app.js
+++ app.js
@@ -6,26 +6,30 @@ app.set('view engine', 'ejs');
 
 
 
-data = {} 
+stats = {} 
 
 app.get('/', async (req, res) => {
-
-  // try {
-  //     const response = await fetch("https://api.brawlstars.com/v1/players/%23VUGVJYUY", {
-  //         method: 'GET',
-  //         headers: {
-  //             Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijc3ZGUwOTYyLWIxMDMtNDA2NC05ODgwLTQyZjMwY2ZjZDUyYSIsImlhdCI6MTY5NjA5NzI2OCwic3ViIjoiZGV2ZWxvcGVyL2NiYmIwMjA0LWNlNWUtY2UwMS1kMDAzLTIzNDgzZjFhNzg3MyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiODkuODQuMjAxLjIyIl0sInR5cGUiOiJjbGllbnQifV19.cLHY9Vq-BUnHtrNVTJ-TGW2O3LsONOEBd6JhBuFPeCTlK-IAGYRWIU61nFv6T3ody2N6ZTLy94mKRX-NpoKEqQ',
-  //             Accept: 'application/json'
-  //         }
-  //     });
-  //     data = await response.json();
-
-  // } catch (error) {
-  //     console.error(error);
-  //     res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
-  // }
-
   res.render('vue');
+});
+
+app.get('/api', async (req, res) => {
+
+   try {
+       const response = await fetch("https://api.brawlstars.com/v1/players/%23VUGVJYUY", {
+           method: 'GET',
+           headers: {
+               Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijc3ZGUwOTYyLWIxMDMtNDA2NC05ODgwLTQyZjMwY2ZjZDUyYSIsImlhdCI6MTY5NjA5NzI2OCwic3ViIjoiZGV2ZWxvcGVyL2NiYmIwMjA0LWNlNWUtY2UwMS1kMDAzLTIzNDgzZjFhNzg3MyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiODkuODQuMjAxLjIyIl0sInR5cGUiOiJjbGllbnQifV19.cLHY9Vq-BUnHtrNVTJ-TGW2O3LsONOEBd6JhBuFPeCTlK-IAGYRWIU61nFv6T3ody2N6ZTLy94mKRX-NpoKEqQ',
+               Accept: 'application/json'
+           }
+       });
+       stats = await response.json();
+
+   } catch (error) {
+       console.error(error);
+      res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
+   }
+
+  res.render('vue',{data:stats});
 });
 
 app.listen(port, () => {
