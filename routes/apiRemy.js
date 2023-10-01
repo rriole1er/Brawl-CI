f31a65f7a58897e07cd01ef0bbd5b74a2f84fd85
Remyto667 <remyriole@hotmail.fr>
2023-10-02 00:11:09 +0200

    fix : apikeyv2

--- routes/apiRemy.js
+++ routes/apiRemy.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch');
 
 stats = {} 
+const proxyUrl = 'http://fixie:kmzzVSUwFDuEsju@velodrome.usefixie.com:80'; // Remplacez par l'URL du proxy Fixie Sock
 
 
 router.get('/', async (req, res) => {
@@ -12,7 +14,8 @@ router.get('/', async (req, res) => {
            headers: {
                Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjVhMjAzYzE3LWFkMmYtNDFkZi1iMDI0LWRmMjc2ZmE1YmU0NyIsImlhdCI6MTY5NjE5NTM3OSwic3ViIjoiZGV2ZWxvcGVyL2NiYmIwMjA0LWNlNWUtY2UwMS1kMDAzLTIzNDgzZjFhNzg3MyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMzQuMTkyLjMxLjg5IiwiMzQuMTkyLjM3LjEwOCIsIjg5Ljg0LjIwMS4yMiIsIjg5LjgxLjE0MC41NyJdLCJ0eXBlIjoiY2xpZW50In1dfQ.O5XEPMRJXCPEwxceDHCxz5q6rKjggf4w9Ds5Hm-SMPxsCgAHfTB_VnkxUKNYDLqCJpaeObLJ61LVjszm5OUwrw' ,
                Accept: 'application/json'
-           }
+           },
+           agent: new HttpsProxyAgent(proxyUrl)
        });
        stats = await response.json();
 
