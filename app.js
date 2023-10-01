b36074c371b150c5fd5bbc34769b5cff4859b9c8
Remyto667 <remyriole@hotmail.fr>
2023-10-01 12:17:03 +0200

    fix : view

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 const express = require('express')
 const app = express()
 const port = 3000
+let ejs = require('ejs');
+app.set('view engine', 'ejs');
+
+
+
+data = {} 
 
 app.get('/', async (req, res) => {
-  try {
-      const response = await fetch("https://api.brawlstars.com/v1/players/%23VUGVJYUY", {
-          method: 'GET',
-          headers: {
-              Authorization: 'Bearer REDACTED_API_KEY',
-              Accept: 'application/json'
-          }
-      });
-      const data = await response.json();
 
-      //console.log(data.tag);
-      //console.log(data)
+  // try {
+  //     const response = await fetch("https://api.brawlstars.com/v1/players/%23VUGVJYUY", {
+  //         method: 'GET',
+  //         headers: {
+  //             Authorization: 'Bearer REDACTED_API_KEY',
+  //             Accept: 'application/json'
+  //         }
+  //     });
+  //     data = await response.json();
 
-      //res.render('vue', { data: data });
+  // } catch (error) {
+  //     console.error(error);
+  //     res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
+  // }
 
-      res.json(data);
-  } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
-  }
-
+  res.render('vue');
 });
 
 app.listen(port, () => {
