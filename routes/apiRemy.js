d0b379f32dce7a2332303b712103dff00067cff7
Remyto667 <remyriole@hotmail.fr>
2023-10-01 13:54:35 +0200

    feat : home page

---
+++ routes/apiRemy.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+stats = {} 
+
+
+router.get('/', async (req, res) => {
+
+   try {
+       const response = await fetch("https://api.brawlstars.com/v1/players/%23VUGVJYUY", {
+           method: 'GET',
+           headers: {
+               Authorization: 'Bearer REDACTED_API_KEY',
+               Accept: 'application/json'
+           }
+       });
+       stats = await response.json();
+
+   } catch (error) {
+       console.error(error);
+      res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
+   }
+
+  res.render('vue',{data:stats});
+});
+
+module.exports = router;
