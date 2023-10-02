aba300b5943b29aad07e73cdca31a3322ad06951
Remyto667 <remyriole@hotmail.fr>
2023-10-02 13:05:54 +0200

    feat : cache api requests 6 hours

--- routes/apiLucas.js
+++ routes/apiLucas.js
@@ -9,7 +9,8 @@ const fixieAuth = fixieUrl.auth.split(':');
 
 const apiKey = 'REDACTED_API_KEY';  // Remplacez par votre clé d'API Brawl Stars
 
-router.get('/proxy', async (req, res) => {
+// Fonction pour récupérer les données depuis l'API Brawl Stars
+async function fetchDataFromBrawlStars() {
     try {
         const response = await axios.get("https://api.brawlstars.com/v1/players/%2320GGQPVVL", {
             headers: {
@@ -26,31 +27,48 @@ router.get('/proxy', async (req, res) => {
                 }
             }
         });
-        const stats = response.data;
-        console.log(stats);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+// Route pour obtenir les données Brawl Stars
+router.get('/proxy', async (req, res) => {
+    try {
+        // Vérifiez si les données sont en cache
+        const cachedData = cache.get(cacheKey);
+
+        if (cachedData) {
+            // Si les données sont en cache, renvoyez-les sans faire de nouvelle requête
+            console.log('Données en cache');
+            res.render('vue', { data: cachedData });
+        } else {
+            // Si les données ne sont pas en cache, récupérez-les depuis l'API Brawl Stars
+            const stats = await fetchDataFromBrawlStars();
+            
+            // Mettez les données en cache pour les prochaines 6 heures (ou votre délai souhaité)
+            cache.set(cacheKey, stats, 6 * 60 * 60); // Cache pendant 6 heures
+            
+            res.render('vue', { data: stats });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
+    }
+});
+
+// La route '/' pour récupérer les données sans utiliser le proxy
+router.get('/', async (req, res) => {
+    try {
+        // Utilisez votre fonction fetchDataFromBrawlStars pour récupérer les données sans utiliser le proxy
+        const stats = await fetchDataFromBrawlStars();
+
         res.render('vue', { data: stats });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
     }
 });
-
-router.get('/', async (req, res) => {
-    try {
-        const response = await fetch("https://api.brawlstars.com/v1/players/%2320GGQPVVL", {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${apiKey}`,
-                Accept: 'application/json'
-            }
-        });
-        stats = await response.json();
-    } catch (error) {
-        console.error(error);
-       res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
-    }
-   
-   res.render('vue',{data:stats});
- });
 
 module.exports = router;
--- routes/apiRemy.js
+++ routes/apiRemy.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const NodeCache = require('node-cache'); // Utilisez un module de cache comme node-cache
 const url = require('url');
-stats = {} 
 
 const fixieUrl = url.parse(process.env.FIXIE_URL || 'http://fixie:kmzzVSUwFDuEsju@velodrome.usefixie.com');
 const fixieAuth = fixieUrl.auth.split(':');
-
 const apiKey = 'REDACTED_API_KEY';  // Remplacez par votre clé d'API Brawl Stars
 
-router.get('/proxy', async (req, res) => {
+const cache = new NodeCache();
+const cacheKey = 'brawl-stars-data'; // Clé de cache pour les données Brawl Stars
+
+// Fonction pour récupérer les données depuis l'API Brawl Stars
+async function fetchDataFromBrawlStars() {
     try {
         const response = await axios.get("https://api.brawlstars.com/v1/players/%23VUGVJYUY", {
             headers: {
@@ -26,8 +29,43 @@ router.get('/proxy', async (req, res) => {
                 }
             }
         });
-        const stats = response.data;
-        console.log(stats);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+// Route pour obtenir les données Brawl Stars
+router.get('/proxy', async (req, res) => {
+    try {
+        // Vérifiez si les données sont en cache
+        const cachedData = cache.get(cacheKey);
+
+        if (cachedData) {
+            // Si les données sont en cache, renvoyez-les sans faire de nouvelle requête
+            console.log('Données en cache');
+            res.render('vue', { data: cachedData });
+        } else {
+            // Si les données ne sont pas en cache, récupérez-les depuis l'API Brawl Stars
+            const stats = await fetchDataFromBrawlStars();
+            
+            // Mettez les données en cache pour les prochaines 6 heures (ou votre délai souhaité)
+            cache.set(cacheKey, stats, 6 * 60 * 60); // Cache pendant 6 heures
+            
+            res.render('vue', { data: stats });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
+    }
+});
+
+// La route '/' pour récupérer les données sans utiliser le proxy
+router.get('/', async (req, res) => {
+    try {
+        // Utilisez votre fonction fetchDataFromBrawlStars pour récupérer les données sans utiliser le proxy
+        const stats = await fetchDataFromBrawlStars();
+
         res.render('vue', { data: stats });
     } catch (error) {
         console.error(error);
@@ -35,27 +73,4 @@ router.get('/proxy', async (req, res) => {
     }
 });
 
-
-router.get('/', async (req, res) => {
-    try {
-        const response = await fetch("https://api.brawlstars.com/v1/players/%23VUGVJYUY", {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${apiKey}`,
-                Accept: 'application/json'
-            }
-        });
-        stats = await response.json();
-    } catch (error) {
-        console.error(error);
-       res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
-    }
-   
-   res.render('vue',{data:stats});
- });
-
-
 module.exports = router;
-
-
-
