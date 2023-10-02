e42e5aa8356827ddb1585fafa9d359be3c4007e6
Remyto667 <remyriole@hotmail.fr>
2023-10-02 12:47:17 +0200

    feat : local IP and Proxy route

--- routes/apiLucas.js
+++ routes/apiLucas.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const url = require('url');
+stats = {} 
 
 const fixieUrl = url.parse(process.env.FIXIE_URL || 'http://fixie:kmzzVSUwFDuEsju@velodrome.usefixie.com');
 const fixieAuth = fixieUrl.auth.split(':');
 
 const apiKey = 'REDACTED_API_KEY';  // Remplacez par votre clé d'API Brawl Stars
 
-router.get('/', async (req, res) => {
+router.get('/proxy', async (req, res) => {
     try {
         const response = await axios.get("https://api.brawlstars.com/v1/players/%2320GGQPVVL", {
             headers: {
@@ -33,5 +34,23 @@ router.get('/', async (req, res) => {
         res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
     }
 });
+
+router.get('/', async (req, res) => {
+    try {
+        const response = await fetch("https://api.brawlstars.com/v1/players/%2320GGQPVVL", {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+                Accept: 'application/json'
+            }
+        });
+        stats = await response.json();
+    } catch (error) {
+        console.error(error);
+       res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
+    }
+   
+   res.render('vue',{data:stats});
+ });
 
 module.exports = router;
--- routes/apiRemy.js
+++ routes/apiRemy.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const url = require('url');
+stats = {} 
 
 const fixieUrl = url.parse(process.env.FIXIE_URL || 'http://fixie:kmzzVSUwFDuEsju@velodrome.usefixie.com');
 const fixieAuth = fixieUrl.auth.split(':');
 
 const apiKey = 'REDACTED_API_KEY';  // Remplacez par votre clé d'API Brawl Stars
 
-router.get('/', async (req, res) => {
+router.get('/proxy', async (req, res) => {
     try {
         const response = await axios.get("https://api.brawlstars.com/v1/players/%23VUGVJYUY", {
             headers: {
@@ -34,27 +35,27 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/check-ip', async (req, res) => {
+
+router.get('/', async (req, res) => {
     try {
-        const response = await axios.get('https://httpbin.org/ip', {
-            proxy: {
-                protocol: 'http',
-                host: fixieUrl.hostname,
-                port: fixieUrl.port,
-                auth: {
-                    username: fixieAuth[0],
-                    password: fixieAuth[1]
-                }
+        const response = await fetch("https://api.brawlstars.com/v1/players/%23VUGVJYUY", {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+                Accept: 'application/json'
             }
         });
-
-        const ipUsedByFixie = response.data.origin;
-        console.log("IP utilisée par Fixie:", ipUsedByFixie);
-        res.send(`IP utilisée par Fixie: ${ipUsedByFixie}`);
+        stats = await response.json();
     } catch (error) {
         console.error(error);
-        res.status(500).send('Erreur lors de la récupération de l\'IP.');
+       res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
     }
-});
+   
+   res.render('vue',{data:stats});
+ });
+
 
 module.exports = router;
+
+
+
