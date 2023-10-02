fa29fd4c6e89e01e0a2c92125a3257623d2abae5
Remyto667 <remyriole@hotmail.fr>
2023-10-02 12:47:17 +0200

    feat : local IP and Proxy route

--- routes/apiRemy.js
+++ routes/apiRemy.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const url = require('url');
+stats = {} 
 
 const fixieUrl = url.parse(process.env.FIXIE_URL || 'http://fixie:kmzzVSUwFDuEsju@velodrome.usefixie.com');
 const fixieAuth = fixieUrl.auth.split(':');
 
-const apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjE5NGY3N2RjLTk2MWItNGZhYS05ZDI3LTU1YzQ3MGEwZGExZiIsImlhdCI6MTY5NjIzNTM1MSwic3ViIjoiZGV2ZWxvcGVyLzFkZGE3MmEzLTFmN2ItZGI4ZS0wMjBlLTk4ODM2ZTI5NzNhNyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiNTQuMTczLjIyOS4yMDAiLCI1NC4xNzUuMjMwLjI1MiJdLCJ0eXBlIjoiY2xpZW50In1dfQ.ON07d6jh1qNu64Dge6Nbvm4YOa3ZO6RePyhs_19GJBFEw3dts-PbQfkgEeDJgwDWrdw4LxvAAStQvIA6jpF6KQ';  // Remplacez par votre clé d'API Brawl Stars
+const apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFlNGNkYjFjLTk5NGUtNGI3OS1iOTU0LWM5NDdkMDRiN2RhYiIsImlhdCI6MTY5NjI0MzM0MCwic3ViIjoiZGV2ZWxvcGVyL2NiYmIwMjA0LWNlNWUtY2UwMS1kMDAzLTIzNDgzZjFhNzg3MyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiODkuODEuMTQwLjU3IiwiODkuODQuMjAxLjIyIiwiNTQuMTczLjIyOS4yMDAiLCI1NC4xNzUuMjMwLjI1MiJdLCJ0eXBlIjoiY2xpZW50In1dfQ.iMye8z_CTbpWMtnFEiJXrrZx-xXJfDeB5sS7fTVI31Qp9Jwj1-cbs88hfGSVZrNsxq8h0ExZYGrT_d3w4IJngQ';  // Remplacez par votre clé d'API Brawl Stars
 
-router.get('/', async (req, res) => {
+router.get('/proxy', async (req, res) => {
     try {
         const response = await axios.get("https://api.brawlstars.com/v1/players/%23VUGVJYUY", {
             headers: {
@@ -34,27 +35,27 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/check-ip', async (req, res) => {
+
+router.get('/', async (req, res) => {
     try {
-        const response = await axios.get('https://httpbin.org/ip', {
-            proxy: {
-                protocol: 'http',
-                host: fixieUrl.hostname,
-                port: fixieUrl.port,
-                auth: {
-                    username: fixieAuth[0],
-                    password: fixieAuth[1]
-                }
+        const response = await fetch("https://api.brawlstars.com/v1/players/%23VUGVJYUY", {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+                Accept: 'application/json'
             }
         });
-
-        const ipUsedByFixie = response.data.origin;
-        console.log("IP utilisée par Fixie:", ipUsedByFixie);
-        res.send(`IP utilisée par Fixie: ${ipUsedByFixie}`);
+        stats = await response.json();
     } catch (error) {
         console.error(error);
-        res.status(500).send('Erreur lors de la récupération de l\'IP.');
+       res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
     }
-});
+   
+   res.render('vue',{data:stats});
+ });
+
 
 module.exports = router;
+
+
+
