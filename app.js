a418d5e627b47ac691ffd2d62c5564035d30c1b9
LeDinoZia <zia.dubois@gmail.com>
2023-10-02 17:00:31 +0200

    fix recupTrophee

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cron = require('node-cron');
+const fetchAndUpdateData = require('./recupererTrophee');
+
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
@@ -10,10 +13,12 @@ const apiRemyRoutes = require('./routes/apiRemy');   // Importez le routeur API
 const apiLucasRoutes = require('./routes/apiLucas');   // Importez le routeur API
 
 
-app.use(express.static(__dirname + '/public/src'));
+app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
 app.use('/', mainRoutes); // Utilisez le routeur principal pour les routes principales
 app.use('/remy', apiRemyRoutes);  // Utilisez le routeur API pour les routes API
 app.use('/lucas', apiLucasRoutes);  // Utilisez le routeur API pour les routes API
+
+cron.schedule('0 0 * * *', fetchAndUpdateData);
--- recupererTrophees.js
+++ recupererTrophees.js
@@ -1,22 +1,28 @@
+const { fetchDataFromBrawlStars } = require('./public/js/apicall');
+
 const fetch = require('node-fetch');
 const fs = require('fs');
+const axios = require('axios');
+const url = require('url');
+const NodeCache = require('node-cache');
+const cache = new NodeCache();
+const cacheKey = 'brawl-stars-data';
 
 async function obtenirTrophees(tagJoueur) {
-    const reponse = await fetch(`https://api.brawlstars.com/v1/players/${tagJoueur}`, {
-        method: 'GET',
-        headers: {
-            Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjE5NGY3N2RjLTk2MWItNGZhYS05ZDI3LTU1YzQ3MGEwZGExZiIsImlhdCI6MTY5NjIzNTM1MSwic3ViIjoiZGV2ZWxvcGVyLzFkZGE3MmEzLTFmN2ItZGI4ZS0wMjBlLTk4ODM2ZTI5NzNhNyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiNTQuMTczLjIyOS4yMDAiLCI1NC4xNzUuMjMwLjI1MiJdLCJ0eXBlIjoiY2xpZW50In1dfQ.ON07d6jh1qNu64Dge6Nbvm4YOa3ZO6RePyhs_19GJBFEw3dts-PbQfkgEeDJgwDWrdw4LxvAAStQvIA6jpF6KQ',
-            Accept: 'application/json'
-        }
-    });
-    
-    if (!reponse.ok) {
-        console.error(`Erreur lors de la récupération des données pour le tag ${tagJoueur}. Code de statut : ${reponse.status}`);
-        return null;
+    const cachedData = cache.get(cacheKey);
+    if (cachedData) {
+        // Si les données sont en cache, renvoyez-les sans faire de nouvelle requête
+        console.log('Données en cache');
+        return cachedData;
+    } else {
+        // Si les données ne sont pas en cache, récupérez-les depuis l'API Brawl Stars
+        const stats = await fetchDataFromBrawlStars(tagJoueur);
+        
+        // Mettez les données en cache pour les prochaines 6 heures (ou votre délai souhaité)
+        cache.set(cacheKey, stats, 6 * 60 * 60);
+        
+        return stats.trophies;
     }
-
-    const donnees = await reponse.json();
-    return donnees.trophies;
 }
 
 function lireFichierJSON(nomFichier) {
@@ -39,60 +45,24 @@ function ecrireJSONDansFichier(nomFichier, donneesJSON) {
     fs.writeFileSync(nomFichier, chaineDonnees);
 }
 
-async function mettreAJourFichierTrophees() {
-    const tagJoueur = "%2320GGQPVVL";
+async function mettreAJourFichierTrophees(tagJoueur, nomFichier) {
     const trophées = await obtenirTrophees(tagJoueur);
-    const donneesJSON = lireFichierJSON('tropheesLuc4gbox.json');
+    const donneesJSON = lireFichierJSON(nomFichier);
     const donneesMisesAJour = ajouterDonneesAuJSON(donneesJSON, trophées);
-    ecrireJSONDansFichier('tropheesLuc4gbox.json', donneesMisesAJour);
+    ecrireJSONDansFichier(nomFichier, donneesMisesAJour);
 }
 
-mettreAJourFichierTrophees();
-
-async function obtenirTrophees2(tagJoueur) {
-    const reponse = await fetch(`https://api.brawlstars.com/v1/players/${tagJoueur}`, {
-        method: 'GET',
-        headers: {
-            Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjE5NGY3N2RjLTk2MWItNGZhYS05ZDI3LTU1YzQ3MGEwZGExZiIsImlhdCI6MTY5NjIzNTM1MSwic3ViIjoiZGV2ZWxvcGVyLzFkZGE3MmEzLTFmN2ItZGI4ZS0wMjBlLTk4ODM2ZTI5NzNhNyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiNTQuMTczLjIyOS4yMDAiLCI1NC4xNzUuMjMwLjI1MiJdLCJ0eXBlIjoiY2xpZW50In1dfQ.ON07d6jh1qNu64Dge6Nbvm4YOa3ZO6RePyhs_19GJBFEw3dts-PbQfkgEeDJgwDWrdw4LxvAAStQvIA6jpF6KQ',
-            Accept: 'application/json'
-        }
+function fetchAndUpdateData() {
+    Promise.all([
+        mettreAJourFichierTrophees("20GGQPVVL", 'tropheesLuc4gbox.json'),
+        mettreAJourFichierTrophees("VUGVJYUY", 'tropheesElRemyto.json')
+    ]).then(() => {
+        console.log("Les fichiers de trophées ont été mis à jour.");
+    }).catch((err) => {
+        console.error("Erreur lors de la mise à jour des fichiers de trophées:", err);
     });
-    
-    if (!reponse.ok) {
-        console.error(`Erreur lors de la récupération des données pour le tag ${tagJoueur}. Code de statut : ${reponse.status}`);
-        return null;
-    }
-
-    const donnees = await reponse.json();
-    return donnees.trophies;
 }
 
-function lireFichierJSON2(nomFichier) {
-    if (fs.existsSync(nomFichier)) {
-        const donneesBrutes = fs.readFileSync(nomFichier, 'utf8');
-        return JSON.parse(donneesBrutes);
-    } else {
-        return {};
-    }
-}
+fetchAndUpdateData();
 
-function ajouterDonneesAuJSON2(donneesJSON, trophees) {
-    const aujourdHui = new Date().toISOString().split('T')[0];
-    donneesJSON[aujourdHui] = trophees;
-    return donneesJSON;
-}
-
-function ecrireJSONDansFichier2(nomFichier, donneesJSON) {
-    const chaineDonnees = JSON.stringify(donneesJSON, null, 2);
-    fs.writeFileSync(nomFichier, chaineDonnees);
-}
-
-async function mettreAJourFichierTrophees2() {
-    const tagJoueur = "%23VUGVJYUY";
-    const trophées = await obtenirTrophees2(tagJoueur);
-    const donneesJSON = lireFichierJSON2('tropheesElRemyto.json');
-    const donneesMisesAJour = ajouterDonneesAuJSON2(donneesJSON, trophées);
-    ecrireJSONDansFichier2('tropheesElRemyto.json', donneesMisesAJour);
-}
-
-mettreAJourFichierTrophees2();
+module.exports = fetchAndUpdateData;
