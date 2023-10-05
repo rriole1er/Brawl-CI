370ee4a40c9dd5cb6ce255fe964a53eea773f08a
LeDinoZia <146435961+LeDinoZia@users.noreply.github.com>
2023-10-05 23:17:05 +0200

    Update app.js : port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 const cron = require('node-cron');
 
 const { fetchAndUpdateData } = require('./public/js/recupererTrophees');
