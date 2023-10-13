8386f6d6e79c4faa6f5c8deeb10c03562e41ca70
Luc4gbox <lucas.dubois753951@gmail.com>
2023-10-14 00:24:02 +0200

    Pourcentage Victoire

--- public/js/apicall.js
+++ public/js/apicall.js
@@ -23,4 +23,21 @@ async function fetchDataFromBrawlStarsLocal(playerTag) {
     }
 }
 
-module.exports = { fetchDataFromBrawlStarsLocal };
+async function fetchDataFromBattleLog(playerTag) {
+
+    try {
+        const response = await fetch(`https://api.brawlstars.com/v1/players/%23${playerTag}/battlelog`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+                Accept: 'application/json'
+            }
+        });
+        return response.json();
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+module.exports = { fetchDataFromBrawlStarsLocal, fetchDataFromBattleLog };
